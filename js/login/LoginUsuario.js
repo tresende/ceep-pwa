dec16e97651dc7a33122780e549d7889bb433f2f
Thiago Resende <thiago@Thiagos-MacBook-Pro.local>
2018-02-12 22:26:18 -0200

    Aula 03 - Salvando cartões com localStorage na adição

--- js/login/LoginUsuario.js
+++ js/login/LoginUsuario.js
@@ -1,4 +1,7 @@
 let logado = JSON.parse(localStorage.getItem("logado"));
+let usuario = localStorage.getItem("usuario");
+let login = new EventEmitter2();
+
 LoginUsuario_render({
     logado: logado,
     usuario : localStorage.getItem("nomeUsuario"),
@@ -6,10 +9,13 @@ LoginUsuario_render({
         logado = true;
         localStorage.setItem("logado", true);
         localStorage.setItem("nomeUsuario", nomeUsuario);
+        usuario = nomeUsuario;
+        login.emit('login');
     },
     onLogout: () => {
         logado = false
         localStorage.setItem("logado", false);
         localStorage.removeItem("nomeUsuario");
+        login.emit('logout');
     }
 });
--- js/mural/Mural.js
+++ js/mural/Mural.js
@@ -1,22 +1,57 @@
 const Mural = (function (_render, Filtro) {
     "use strict"
-    let cartoes = []
+    let cartoes = pegaCartoesUsuario();
+    cartoes.forEach(cartao => {
+        preparaCartao(cartao);
+    })
+
     const render = () => _render({ cartoes: cartoes, filtro: Filtro.tagsETexto });
+    render()
 
     Filtro.on("filtrado", render)
 
+    function salvaCartoes() {
+        localStorage.setItem(usuario, JSON.stringify(
+            cartoes.map(cartao => ({ conteudo: cartao.conteudo, tipo: cartao.tipo }))
+        ))
+    }
+
+    login.on('login', () => {
+        cartoes = pegaCartoesUsuario();
+        render();
+    })
+
+    login.on('logout', () => {
+        cartoes = [];
+        render();
+    })
+
+    function pegaCartoesUsuario() {
+        let cartoesLocal = JSON.parse(localStorage.getItem(usuario));
+        if (cartoesLocal) {
+            return cartoesLocal.map(cartaoLocal => new Cartao(cartaoLocal.conteudo, cartaoLocal.tipo)) || [];
+        }
+        return []
+    }
+
+    function preparaCartao(cartao) {
+        cartao.on("mudanca.**", salvaCartoes)
+        cartao.on("remocao", () => {
+            cartoes = cartoes.slice(0);
+            cartoes.splice(cartoes.indexOf(cartao), 1)
+            salvaCartoes()
+            render()
+        })
+    }
     function adiciona(cartao) {
         if (logado) {
-            cartoes.push(cartao)
+            cartoes.push(cartao);
+            salvaCartoes()
             cartao.on("mudanca.**", render)
-            cartao.on("remocao", () => {
-                cartoes = cartoes.slice(0)
-                cartoes.splice(cartoes.indexOf(cartao), 1)
-                render()
-            })
+            preparaCartao(cartao);
             render()
             return true
-        }else{
+        } else {
             alert('Voce nao está logado')
         }
     }
