933c2cc2a09ad6fa40bc867b4e73e88d1d8128a8
Thiago Resende <thiago@Thiagos-MacBook-Pro.local>
2018-02-13 16:48:14 -0200

    Aula 04 - Recursos não offline com App Cache

--- js/mural/Mural.js
+++ js/mural/Mural.js
@@ -35,6 +35,14 @@ const Mural = (function (_render, Filtro) {
     }
 
     function preparaCartao(cartao) {
+        let urlsImagens = Cartao.pegaImagens(cartao);
+        urlsImagens.forEach(url => {
+            fetch(url).then(resposta => {
+                caches.open('ceep-imagens').then(cache =>{
+                    cache.put(url, resposta);
+                })
+            });
+        })
         cartao.on("mudanca.**", salvaCartoes)
         cartao.on("remocao", () => {
             cartoes = cartoes.slice(0);
